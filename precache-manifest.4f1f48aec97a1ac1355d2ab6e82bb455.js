056542f42b29abd74607e428d9b2b9c2f0ecdf34
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2021-07-17 15:44:48 +0200

    Updates

---
+++ precache-manifest.4f1f48aec97a1ac1355d2ab6e82bb455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b55a4c494a84852769b268fcd58bb57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf14faf8fc8f92b108c5",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "291f471688b0108d96fa",
+    "url": "/static/js/2.33ef47b7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.33ef47b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf14faf8fc8f92b108c5",
+    "url": "/static/js/main.f3722ff3.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6a985de41018af09d2cbee295d65b41d",
+    "url": "/static/media/erlantzcalvo.6a985de4.jpg"
+  },
+  {
+    "revision": "b737fc6adcbd71c2b89c8046505a2bd8",
+    "url": "/static/media/resume.b737fc6a.pdf"
+  }
+]);
