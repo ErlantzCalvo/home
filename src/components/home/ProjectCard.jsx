b2ba89f4593178dca30c798c0f1bd92d2472ae95
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2023-10-19 17:59:37 +0200

    Add silent-link styles

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -4,7 +4,7 @@ import Card from "react-bootstrap/Card";
 const ProjectCard = ({project}) =>{
     return(
         <Card style={{'width': '18em'}}>
-            <a href={project.url} target=" _blank" style={{'color': 'unset'}}>
+            <a href={project.url} target=" _blank" className="silent-link">
                 <Card.Img variant="top" src={"/home/projects/" + project.image} alt={project.name} />
                 <Card.Body>
                     <Card.Title>{project.name}</Card.Title>
