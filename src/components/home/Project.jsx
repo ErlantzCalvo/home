6be665215fd0330388da07ad1b3db69c89f6b3de
Erlantz Calvo <erlantzcalvocarrillo@gmail.com>
2023-10-19 17:55:00 +0200

    Add live projects

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,100 +1,23 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import ProjectCard from "./ProjectCard";
-import axios from "axios";
 
-const dummyProject = {
-  name: null,
-  description: null,
-  svn_url: null,
-  stargazers_count: null,
-  languages_url: null,
-  pushed_at: null,
-};
-const API = "https://api.github.com";
+const Project = ({heading, projects}) => {
 
-const Project = ({ heading, username, length, specfic }) => {
-  const allReposAPI = `${API}/users/${username}/repos?sort=updated&direction=desc`;
-  const specficReposAPI = `${API}/repos/${username}`;
-  const dummyProjectsArr = new Array(length + specfic.length).fill(
-    dummyProject
-  );
-
-  const [projectsArray, setProjectsArray] = useState([]);
-
-  const fetchRepos = useCallback(async () => {
-    let repoList = [];
-    let privateRepos = [{
-        name: "House-pricing viewer",
-        description: "View housing prices in Spain updated daily. \n\nThis project was made in order to learn Go, React and Next.js.\n" +
-         "A Webscraper made in Golang retrieves the euribor value and the housing information from multiple spanish housing web pages every morning and stores it " +
-         "in Google Firestore. This page built with Next.js then shows the saved data.",
-        svn_url: "https://hay-que-vivir.vercel.app/",
-        stargazers_count: null,
-        languages_url: null,
-        pushed_at: "2022-10-22T16:39:15Z",
-        is_private: true
-  }];
-    try {
-      // getting all repos
-      const response = await axios.get(allReposAPI);
-      // order by stargazers
-      // response.data.sort((a, b) => {
-      //   return b.stargazers_count - a.stargazers_count;
-      // })
-
-      // slicing to the length
-      repoList = [...response.data.slice(0, length)];
-      // adding specified repos
-      try {
-        for (let repoName of specfic) {
-          const response = await axios.get(`${specficReposAPI}/${repoName}`);
-          repoList.push(response.data);
-        }
-        // repoList = [...repoList, privateRepos]
-        repoList = privateRepos.concat(repoList)
-      } catch (error) {
-        console.error(error.message);
-      }
-      // setting projectArray
-      // TODO: remove the duplication.
-      console.log(repoList)
-      setProjectsArray(repoList);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }, [allReposAPI, length, specfic, specficReposAPI]);
-
-  useEffect(() => {
-    fetchRepos();
-  }, [fetchRepos]);
-
-  return (
-    <Jumbotron fluid id="projects" className="bg-light m-0">
-      <Container className="">
-        <h2 className="display-4 pb-5 text-center">{heading}</h2>
-        <Row>
-          {projectsArray.length
-            ? projectsArray.map((project, index) => (
-                <ProjectCard
-                  key={`project-card-${index}`}
-                  id={`project-card-${index}`}
-                  value={project}
-                />
-              ))
-            : dummyProjectsArr.map((project, index) => (
-                <ProjectCard
-                  key={`dummy-${index}`}
-                  id={`dummy-${index}`}
-                  value={project}
-                />
-              ))}
-        </Row>
-      </Container>
-    </Jumbotron>
-  );
+    return (
+        <Jumbotron fluid id="projects" className="bg-light m-0">
+        <Container className="">
+          <h2 className="display-4 pb-5 text-center">{heading}</h2>
+          <Row>
+           {projects.map((project, index) => (
+            <ProjectCard key={'project-' + index} project={project}/>
+           ))}
+          </Row>
+        </Container>
+      </Jumbotron>
+    );
 };
 
 export default Project;
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -50,42 +50,33 @@ const about = {
   resume: require("../editable-stuff/resume.pdf"),
 };
 
-// PROJECTS SECTION
+const projects = {
+  show: true,
+  heading: "Projects", 
+  projectsDetails: [
+    {
+      name: "HayQueVivir",
+      url: "https://hay-que-vivir.vercel.app",
+      image: "HayQueVivir.jpg",
+      stack: ["Go", "Next.js", "Tailwind", "Firebase"],
+      description: "Follow the house prices in the different provinces of Spain with daily updated prices.",
+      technical_details: "This project is separated into two different parts:\n\nOn the one hand there is a process made with Golang that is in charge of obtaining the statistics of the daily house prices as well as the Euribor. Both data are obtained through the use of webscraping and are stored in Firestore.\nOn the other hand, there is the web that users see, which is a web application created with Next.js that obtains the data saved by the webscraper in Firestore."
+    }
+  ],
+};
+
+// GITHUB SECTION
 // Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
 //      i.e: reposLength: 0,
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
   show: true,
-  heading: "Recent Projects",
+  heading: "Github Projects",
   gitHubUsername: "ErlantzCalvo", 
   reposLength: 4,
   specificRepos: [],
 };
-
-// Leadership SECTION
-// const leadership = {
-//   show: false,
-//   heading: "Leadership",
-//   message:
-//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-//   images: [
-//     { 
-//       img: require("../editable-stuff/hashirshoaeb.png"), 
-//       label: "First slide label", 
-//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-//     },
-//     { 
-//       img: require("../editable-stuff/hashirshoaeb.png"), 
-//       label: "Second slide label", 
-//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-//     },
-//   ],
-//   imageSize: {
-//     width:"615",
-//     height:"450"
-//   }
-// };
 
 // SKILLS SECTION
 const skills = {
@@ -122,26 +113,5 @@ const getInTouch = {
   email: "erlantzcalvocarrillo@gmail.com",
 };
 
-// const experiences = {
-//   show: false,
-//   heading: "Experiences",
-//   data: [
-//     {
-//       role: 'Software Engineer',// Here Add Company Name
-//       companylogo: require('../assets/img/dell.png'),
-//       date: 'June 2018 – Present',
-//     },
-//     {
-//       role: 'Front-End Developer',
-//       companylogo: require('../assets/img/boeing.png'),
-//       date: 'May 2017 – May 2018',
-//     },
-//   ]
-// }
 
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
-
-export { navBar, mainBody, about, repos, skills, /*leadership,*/ getInTouch/*, experiences*/ };
+export { navBar, mainBody, about, projects, repos, skills, /*leadership,*/ getInTouch/*, experiences*/ };
